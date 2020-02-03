a0f166df6940f3d57b22c15177a9784ca9a126bb
Raphaël Bournhonesque <raphael@mppteam.com>
2020-02-03 19:13:33 +0100

    Add a redirect from / to /questions

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 
 
 const routes = [
-  { path: '/', component: QuestionView },
+  { path: '/', redirect: '/questions' },
   { path: '/ingredients', component: IngredientsView },
   { path: '/questions', component: QuestionView },
 ];
