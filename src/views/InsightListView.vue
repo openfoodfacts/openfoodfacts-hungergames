63fc356869a0782f2890172969a06acc0324c27e
Raphaël Bournhonesque <raphael@mppteam.com>
2020-02-10 19:29:06 +0100

    Add new fields to insight table

--- src/views/InsightListView.vue
+++ src/views/InsightListView.vue
@@ -1,45 +1,47 @@
 <template>
   <div class="ui container">
-    <h2>Insights</h2>
-    <div class="ui form">
-      <div class="fields">
-        <div class="field">
-          <label>Barcode</label>
-          <input placeholder="Barcode" v-model="barcodeFilter" />
+    <div>
+      <h2>Insights</h2>
+      <div class="ui form">
+        <div class="fields">
+          <div class="field">
+            <label>Barcode</label>
+            <input placeholder="Barcode" v-model="barcodeFilter" />
+          </div>
+          <div class="field">
+            <label>Value tag</label>
+            <input type="text" placeholder="ex: en:mueslis, bjorg,..." v-model="valueTagFilter" />
+          </div>
+          <div class="field">
+            <label>Type</label>
+            <select class="ui search dropdown" v-model="insightTypeFilter">
+              <option value>All</option>
+              <option value="label">Label</option>
+              <option value="product_weight">Product weight</option>
+              <option value="category">Category</option>
+              <option value="expiration_date">Expiration date</option>
+              <option value="packager_code">Packager code</option>
+            </select>
+          </div>
+          <div class="field">
+            <label>Annotated</label>
+            <select class="ui search dropdown" v-model="annotationFilter">
+              <option value>All</option>
+              <option value="-1">Skipped (-1)</option>
+              <option value="0">Rejected (0)</option>
+              <option value="1">Accepted (1)</option>
+              <option value="not_annotated">Not annotated</option>
+            </select>
+          </div>
         </div>
-        <div class="field">
-          <label>Value tag</label>
-          <input type="text" placeholder="ex: en:mueslis, bjorg,..." v-model="valueTagFilter" />
-        </div>
-        <div class="field">
-          <label>Type</label>
-          <select class="ui search dropdown" v-model="insightTypeFilter">
-            <option value>All</option>
-            <option value="label">Label</option>
-            <option value="product_weight">Product weight</option>
-            <option value="category">Category</option>
-            <option value="expiration_date">Expiration date</option>
-            <option value="packager_code">Packager code</option>
-          </select>
-        </div>
-        <div class="field">
-          <label>Annotated</label>
-          <select class="ui search dropdown" v-model="annotationFilter">
-            <option value>All</option>
-            <option value="-1">Skipped (-1)</option>
-            <option value="0">Rejected (0)</option>
-            <option value="1">Accepted (1)</option>
-            <option value="not_annotated">Not annotated</option>
-          </select>
-        </div>
+        <input class="ui submit button primary" type="submit" value="Search" @click="loadInsights" />
       </div>
-      <input class="ui submit button primary" type="submit" value="Search" @click="loadInsights" />
+      <div class="ui divider" />
+      <p>
+        <strong>Count:</strong>
+        {{ resultCount }}
+      </p>
     </div>
-    <div class="ui divider" />
-    <p>
-      <strong>Count:</strong>
-      {{ resultCount }}
-    </p>
     <table class="ui celled table">
       <thead>
         <tr>
@@ -48,7 +50,9 @@
           <th>Type</th>
           <th>Value</th>
           <th>Created at</th>
+          <th>Completed at</th>
           <th>Annotation</th>
+          <th>Automatic</th>
         </tr>
       </thead>
       <tbody>
@@ -57,11 +61,15 @@
           <td data-label="Id">{{ insight.id }}</td>
           <td data-label="Type">{{ insight.type }}</td>
           <td data-label="Value">{{ insight.value || insight.value_tag }}</td>
-          <td data-label="Added">{{ insight.timestamp }}</td>
+          <td data-label="Created at">{{ formatDatetime(insight.timestamp) }}</td>
+          <td data-label="Completed at">{{ formatDatetime(insight.completed_at) }}</td>
           <td data-label="Annotation">
             <i v-if="insight.annotation == 1" class="large green checkmark icon"></i>
             <i v-else-if="insight.annotation == 0" class="large red times icon"></i>
             <i v-else-if="insight.annotation == -1" class="large grey question icon"></i>
+          </td>
+          <td data-label="Automatic">
+            <input type="checkbox" :checked="insight.automatic_processing" disabled />
           </td>
         </tr>
       </tbody>
@@ -106,6 +114,12 @@ export default {
     }
   },
   methods: {
+    formatDatetime: function(datetime) {
+      if (!datetime) {
+        return null;
+      }
+      return datetime.split(".")[0];
+    },
     updatedPage: function() {
       this.loadInsights();
     },
