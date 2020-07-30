66fda782fc3c11af346518885276338d41ecac80
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-07-30 21:05:17 +0200

    load product and show a form

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             <sui-dropdown-header>{{$t('menu.games')}}</sui-dropdown-header>
             <router-link class="item" to="/questions">{{$t('menu.questions')}}</router-link>
             <router-link class="item" to="/logos">{{$t('menu.logos')}}</router-link>
-            <router-link class="item" to="/nutritions">{{$t('menu.logos')}}</router-link>
+            <router-link class="item" to="/nutritions">{{$t('menu.nutritions')}}</router-link>
 
             <sui-dropdown-divider />
             <sui-dropdown-header>{{$t('menu.manage')}}</sui-dropdown-header>
---
+++ src/views/NutritionView.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="ui grid stackable">
+    <div class="five wide column centered">
+      <div class="insight-column">
+        <div class="ui form">
+          <div class="ui icon input" id="value-tag-input">
+            <input
+              class="ui input"
+              placeholder="value (fanta, en:organic,...)"
+              v-model="valueTagInput"
+            />
+            <i
+              @click="clearValueTagInput()"
+              v-if="valueTagInput"
+              class="times link icon"
+            ></i>
+          </div>
+          <!-- <div class="ui toggle checkbox" style="margin-top: 0.5rem;">
+            <input v-model="sortByPopularity" type="checkbox" name="sortBy" />
+            <label>Sort by popularity</label>
+          </div> -->
+        </div>
+        <div class="ui divider hidden"></div>
+        <viewer :options="imageZoomOptions" style="height: 300px">
+          <img
+            :class="[imageRotationClass]"
+            :src="productBuffer[0]['image_nutrition_url']"
+            style="max-height: 300px;
+          max-width: 300px;"
+          />
+        </viewer>
+        <div class="ui divider hidden"></div>
+        <div>
+          <ul class="table">
+            <li
+              v-for="nutritiveValue in currentProductData"
+              :key="nutritiveValue.id"
+              v-bind:class="{ shadow: !nutritiveValue.visible }"
+              class="row"
+            >
+              <p class="cell">
+                {{ nutritiveValue.name }}
+              </p>
+              <div class="cell">
+                <div style="display:flex">
+                  <input
+                    v-model="currentProductData[nutritiveValue.id]['data']"
+                  />
+                  <select
+                    v-if="getNutrimentUnits(nutritiveValue.name).length > 1"
+                    v-model="currentProductData[nutritiveValue.id]['unit']"
+                  >
+                    <option disabled value="">
+                      unit
+                    </option>
+                    <option
+                      v-for="unit in getNutrimentUnits(nutritiveValue.name)"
+                      :key="unit"
+                      v-bind:value="unit"
+                    >
+                      {{ unit }}
+                    </option>
+                  </select>
+                  <span v-else>
+                    {{ getNutrimentUnits(nutritiveValue.name)[0] }}
+                  </span>
+                </div>
+              </div>
+              <div class="cell ui toggle checkbox">
+                <input
+                  type="checkbox"
+                  name="public"
+                  v-model="currentProductData[nutritiveValue.id]['visible']"
+                />
+                <label>Subscribe to weekly newsletter</label>
+              </div>
+            </li>
+          </ul>
+          <!-- <button
+            data-inverted
+            data-tooltip="Shortcut: n"
+            class="ui button red annotate"
+            @click="annotate(0)"
+          >
+            No
+          </button>
+          <button
+            data-inverted
+            data-tooltip="Shortcut: k"
+            class="ui button annotate"
+            @click="annotate(-1)"
+          >
+            Skip
+          </button>
+          <button
+            data-inverted
+            data-tooltip="Shortcut: o"
+            class="ui button green annotate"
+            @click="annotate(1)"
+          >
+            Yes
+          </button> -->
+        </div>
+      </div>
+      <div class="flex-center" style="margin-top: 100px;">
+        <div class="loading-spinner" v-if="loading">
+          <div class="loading-spinner-content">
+            <div></div>
+          </div>
+        </div>
+        <div v-if="noRemainingQuestion">
+          <h2>No questions remaining</h2>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="six wide column centered">
+      <Product :barcode="currentQuestionBarcode" />
+    </div>
+    <div class="three wide column annotation-column">
+      <AnnotationCounter
+        :remainingCount="remainingQuestionCount"
+        :lastAnnotations="lastAnnotations"
+        :sessionAnnotatedCount="sessionAnnotatedCount"
+      />
+    </div> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+// import { localSettings } from "../settings";
+import nutriments from "../data/nutritions";
+import { OFF_URL } from "../const";
+// import { annotate as robotoffAnnotate } from "../robotoff";
+// import Product from "../components/Product";
+// import AnnotationCounter from "../components/AnnotationCounter";
+
+const getProducts = async (nbOfPages) => {
+  const randomPage = Math.floor(Math.random() * nbOfPages);
+  const {
+    data: { products },
+  } = await axios(
+    `${OFF_URL}/state/photos-validated/state/nutrition-facts-to-be-completed/${randomPage}.json?fields=code,lang,image_nutrition_url,product_name
+          `
+  );
+  return products;
+};
+
+export default {
+  name: "NutritionView",
+  components: {},
+  data: function() {
+    return {
+      imageZoomOptions: {
+        toolbar: {
+          rotateLeft: 1,
+          rotateRight: 1,
+        },
+      },
+      loading: false,
+      productBuffer: [],
+      currentProductData: {},
+    };
+  },
+  methods: {
+    addProducts: async function() {
+      this.loading = true;
+      const newProducts = await getProducts(20);
+      this.productBuffer = this.productBuffer.concat(newProducts);
+      this.loading = false;
+    },
+    skipProduct() {
+      this.productBuffer.shift();
+    },
+    resetProductData() {
+      const data = {};
+
+      for (const nutrimentName of Object.keys(nutriments)) {
+        data[nutriments[nutrimentName]] = {
+          id: nutriments[nutrimentName],
+          name: nutrimentName,
+          data: "",
+          unit: "",
+          visible: true,
+        };
+      }
+      this.currentProductData = data;
+    },
+    getNutrimentUnits(nutrimentName) {
+      switch (nutrimentName) {
+        case "Energy (kCal)":
+          return ["kcal"];
+        case "Energy (kJ)":
+          return ["kJ"];
+        default:
+          return ["g", "mg"];
+      }
+    },
+  },
+  watch: {
+    productBuffer: function(newProductBuffer, oldProductBuffer) {
+      if (newProductBuffer.length <= 5 && !this.loading) {
+        this.addProducts();
+      }
+      if (
+        newProductBuffer[0] &&
+        (!oldProductBuffer[0] ||
+          newProductBuffer[0].code !== oldProductBuffer[0].code)
+      ) {
+        //if we have a product to display, and this product just changed
+        this.resetProductData();
+      }
+    },
+  },
+  mounted: function() {
+    this.addProducts();
+  },
+};
+</script>
+
+<style scoped>
+.shadow {
+  opacity: 0.2;
+}
+.table {
+  display: table;
+}
+.row {
+  display: table-row;
+}
+.cell {
+  display: table-cell;
+}
+</style>
