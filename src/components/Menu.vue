25a32a3318095d29ade67871433c13968f2c4a5d
Andrea Stagi <stagi.andrea@gmail.com>
2020-08-01 19:45:08 +0200

    Refactoring

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,16 +3,22 @@
     <sui-dropdown item icon="bars" simple class="mobile only">
       <sui-dropdown-menu>
         <template v-for="item in menu">
-          <sui-dropdown-header v-if="!item.to" :key="item.label">{{item.label}}</sui-dropdown-header>
+          <sui-dropdown-header v-if="!item.to" :key="item.label">
+            {{item.label}}
+          </sui-dropdown-header>
           <sui-dropdown-divider v-if="!item.label" :key="item.label"/>
-          <router-link active-class="active" class="item" :to="item.to" :key="item.label" v-if="item.to">{{item.label}}</router-link>
+          <router-link active-class="active" class="item" :to="item.to" :key="item.label" v-if="item.to">
+            {{item.label}}
+          </router-link>
         </template>
       </sui-dropdown-menu>
     </sui-dropdown>
     <sui-menu-item class="item mobile hidden">
       <img src="~/../assets/logo.png" >
     </sui-menu-item>
-    <router-link v-for="item in menuItems" active-class="active" class="item mobile hidden" :to="item.to" :key='item.to'>{{item.label}}</router-link>
+    <router-link v-for="item in menuItems" active-class="active" class="item mobile hidden" :to="item.to" :key='item.to'>
+      {{item.label}}
+    </router-link>
   </sui-menu>
 </template>
 
