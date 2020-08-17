51771676b5cb9fcdc164ece7679093e72ebdf79e
Andrea Stagi <stagi.andrea@gmail.com>
2020-08-17 12:48:49 +0200

    Use default semantic UI loading spinner

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="loading-spinner-container" v-if="show">
-    <div class="loading-spinner">
-      <div class="loading-spinner-content">
-        <div></div>
-      </div>
-    </div>
+  <div :class="{centered: centered}">
+    <div class="ui active inline loader" v-if="show" :class="[size]"></div>
   </div>
 </template>
 
@@ -15,55 +11,25 @@ export default {
     show: {
       type: Boolean,
       required: true
+    },
+    centered: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'huge'
     }
   }
 };
 </script>
 
 <style scoped>
-@keyframes loading-spinner-content {
-  0% {
-    transform: rotate(0deg);
-  }
-  50% {
-    transform: rotate(180deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-.loading-spinner-container {
+.centered {
   display: flex;
   align-items: center;
   flex-direction: column;
-}
-.loading-spinner-content div {
-  position: absolute;
-  animation: loading-spinner-content 1s linear infinite;
-  width: 80px;
-  height: 80px;
-  top: 10px;
-  left: 10px;
-  border-radius: 50%;
-  box-shadow: 0 4px 0 0 #1d3f72;
-  transform-origin: 40px 41px;
-}
-.loading-spinner {
-  width: 100px;
-  height: 100px;
-  display: inline-block;
-  overflow: hidden;
-  background: #fff;
-}
-.loading-spinner-content {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  transform: translateZ(0) scale(1);
-  backface-visibility: hidden;
-  transform-origin: 0 0; /* see note above */
-}
-.loading-spinner-content div {
-  box-sizing: content-box;
 }
 </style>
