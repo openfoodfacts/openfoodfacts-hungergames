5c774e7b5916b6f087ac182c8d80b3ed3f7b2332
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-02-07 14:39:35 +0100

    open modal for first visit or when help is clicked

---
+++ src/components/Helper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <sui-modal :open="open" v-on:changed="close">
+      <sui-modal-header>Help page</sui-modal-header>
+      <sui-modal-content image>
+        <img wrapped size="large" src="~/../assets/logo.png" />
+        <sui-modal-description>
+          <sui-header>Le logo</sui-header>
+          <p>
+            Ceci est un très joli logo
+          </p>
+          <p>Vous ne trouvez pas qu'il ressemble fort à une loupe ?</p>
+          <p>
+            Il faudrait ici metre un text et une image pour le jeu :
+            {{ $router.history.current.path }}
+          </p>
+        </sui-modal-description>
+      </sui-modal-content>
+      <sui-modal-actions>
+        <sui-button positive @click.native="close">
+          OK
+        </sui-button>
+      </sui-modal-actions>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroductionView",
+  props: { open: Boolean },
+  data: function() {
+    return {
+      currentPath: this.$router.history.current.path,
+    };
+  },
+  methods: {
+    close: function() {
+      this.$emit("close-helper");
+    },
+  },
+};
+</script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -19,10 +19,23 @@
     <router-link v-for="item in menuItems" active-class="active" class="item mobile hidden" :to="item.to" :key='item.to'>
       {{item.label}}
     </router-link>
+
+    <sui-menu-menu position="right">
+      <sui-menu-item v-on:click="openHelper" class="helpButton">
+        <sui-icon name="help" />
+      </sui-menu-item>
+    </sui-menu-menu>
+    <Helper :open="helperIsOpen" v-on:close-helper="closeHelper" />
   </sui-menu>
 </template>
 
 <script>
+import { localSettings } from "../settings";
+import Helper from "./Helper";
+
+const intialSettings = localSettings.fetch();
+localSettings.update("tuto-done", true);
+
 export default {
   data: function() {
     return {
@@ -35,21 +48,35 @@ export default {
         {label: this.$t('menu.manage'), type: 'header'},
         {label: this.$t('menu.insights'), to:'/insights'},
         {label: this.$t('menu.settings'), to:'/settings'},
-      ]
+      ],
+      helperIsOpen: !intialSettings["tuto-done"],
     };
   },
+  components: { Helper },
   computed: {
     menuItems: function() {
       return this.menu.filter((item) => {
         return item.to
       })
     }
-  }
+  },
+  methods: {
+    closeHelper: function() {
+      this.helperIsOpen = false;
+    },
+    openHelper: function() {
+      this.helperIsOpen = true;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .menu-container {
   margin-bottom: 1rem;
+}
+.helpButton:hover {
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.05);
 }
 </style>
