ef206569d55ea409301c6b53b9d183f5597b1878
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-02-25 22:36:56 +0100

    complete the game explanation

--- src/components/Helper.vue
+++ src/components/Helper.vue
@@ -3,23 +3,27 @@
     <sui-modal :open="open" v-on:changed="close">
       <sui-modal-header>Help page</sui-modal-header>
       <sui-modal-content image>
-        <img wrapped size="large" src="~/../assets/logo.png" />
+        <sui-image
+          wrapped
+          :size="helpInformations[pageIndex].imageSize || 'medium'"
+          :src="imgUrl"
+        />
+        <!-- <img wrapped size="large" :src="imgUrl" /> -->
         <sui-modal-description>
-          <sui-header>Le logo</sui-header>
-          <p>
-            Ceci est un très joli logo
-          </p>
-          <p>Vous ne trouvez pas qu'il ressemble fort à une loupe ?</p>
-          <p>
-            Il faudrait ici metre un text et une image pour le jeu :
-            {{ $router.history.current.path }}
+          <sui-header>{{ helpInformations[pageIndex].title }}</sui-header>
+          <p
+            v-for="(text, index) in helpInformations[pageIndex].texts"
+            :key="index"
+            class="explanationText"
+          >
+            {{ text }}
           </p>
         </sui-modal-description>
       </sui-modal-content>
       <sui-segment aligned="right">
-        <sui-progress attached top :percent="50" color="green" />
-        <sui-button @click.native="decrease">prev</sui-button>
-        <sui-button @click.native="increase">next</sui-button>
+        <sui-progress attached top :percent="pageRatio" color="green" />
+        <sui-button @click.native="prev">prev</sui-button>
+        <sui-button @click.native="next">next</sui-button>
 
         <sui-button positive @click.native="close">
           OK
@@ -30,18 +34,120 @@
 </template>
 
 <script>
+// potential image size (default is medium) : "mini" | "tiny" | "small" | "medium" | "large" | "big" | "huge" | "massive"
+const explanations = {
+  welcome: [
+    {
+      title: "Welcome",
+      texts: [
+        "Hunger Games is a collection of mini-games that help contribute to Open Food Facts in many ways. Categorize a product to get the Nutri-Score and Eco-Score, help validate labels for the Eco-Score, add brands and weights, help our logo recognition system learn about the logos it detected.",
+        "Those are some of the exciting things you'll be able to do. Make sure to be connected to the Open Food Facts website to get credit for your contributions. If you make a mistake, please take the time to correct it by heading to the website.",
+        "In case of doubt about the game, you have an helper at the top right of the page, and you can reach us on slack at the #hunger-games channel",
+      ],
+    },
+    {
+      title: "Questions",
+      texts: [
+        "Our AI has a lot of questions for you. Here you will have to confirm or deny the information she detected on images.",
+        "Do not hestiate to skip a question if you're not sure, there are plenty of others",
+      ],
+      image: "questionsScreenshot.png",
+      imageSize: "huge",
+    },
+    {
+      title: "Logos",
+      texts: [
+        "The worlds of products is full of logos. Explain to our AI which one are the same.",
+      ],
+      image: "logoGeneral.png",
+      imageSize: "huge",
+    },
+  ],
+  logos: [
+    {
+      title: "Selection",
+      texts: [
+        "Select the crops of a same logo by clicking on them",
+        "Selected logos go to the top of the page, you can unselect them by clicking again on them",
+      ],
+      image: "logoGeneral.png",
+    },
+    {
+      title: "Warning",
+      texts: [
+        "If the crop contains multiple logo do not select it.",
+        "If the crop contains only a partial part of a logo you can select it",
+      ],
+      image: "memLogo.png",
+    },
+    {
+      title: "Validate",
+      texts: [
+        "Indicate the type of logo (label, brand, QRcode, ...)",
+        "If it is a label or a brand logo, indicate which one it is",
+        "Now you can submit your selection",
+        "Thank you :)",
+      ],
+      image: "logoNameLabel.png",
+      imageSize: "large",
+    },
+  ],
+};
+const getHelpInformations = (currentPath, explanations) => {
+  const currentPathKey = currentPath.slice(1);
+  if (currentPathKey in explanations) {
+    return explanations[currentPathKey];
+  }
+  return explanations["welcome"];
+};
+
 export default {
   name: "IntroductionView",
   props: { open: Boolean },
   data: function() {
     return {
-      currentPath: this.$router.history.current.path,
+      pageIndex: 0,
+      helpInformations: getHelpInformations(
+        this.$router.history.current.path,
+        explanations
+      ),
     };
+  },
+  computed: {
+    pageRatio: function() {
+      if (this.helpInformations.length === 1) {
+        return 100;
+      }
+      return Math.floor(
+        100 * (this.pageIndex / (this.helpInformations.length - 1))
+      );
+    },
+    imgUrl: function() {
+      var images = require.context("../assets/", false, /\.png$/);
+      console.log(this.helpInformations[this.pageIndex].image);
+      return images(
+        "./" + (this.helpInformations[this.pageIndex].image || "logo.png")
+      );
+    },
   },
   methods: {
     close: function() {
       this.$emit("close-helper");
     },
+    prev: function() {
+      this.pageIndex = Math.max(0, this.pageIndex - 1);
+    },
+    next: function() {
+      this.pageIndex = Math.min(
+        this.helpInformations.length - 1,
+        this.pageIndex + 1
+      );
+    },
   },
 };
 </script>
+<style scoped>
+.explanationText {
+  font-size: 1.3rem;
+}
+</style>
