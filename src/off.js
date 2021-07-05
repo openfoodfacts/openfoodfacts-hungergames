8ec7e23da551e6d23f134f0d7d9830e1f4a99acc
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-07-05 21:48:24 +0200

    allows to filter accorting to the country and the user

--- src/off.js
+++ src/off.js
@@ -1,12 +1,13 @@
 import { getLang } from "./settings";
-import { OFF_API_URL, OFF_IMAGE_URL, OFF_URL } from "./const";
-import axios from 'axios';
-import combineURLs from 'axios/lib/helpers/combineURLs';
-
+import { OFF_API_URL, OFF_IMAGE_URL, OFF_SEARCH } from "./const";
+import axios from "axios";
+import combineURLs from "axios/lib/helpers/combineURLs";
 
 export default {
   getCookie(name) {
-    const cookies = document.cookie.split(';').filter((item) => item.trim().startsWith(`${name}=`));
+    const cookies = document.cookie
+      .split(";")
+      .filter((item) => item.trim().startsWith(`${name}=`));
     if (cookies.length) {
       const cookie = cookies[0];
       return cookie.split("=", 2)[1];
@@ -23,7 +24,7 @@ export default {
 
     let isNext = false;
     let username = "";
-    sessionCookie.split("&").forEach(el => {
+    sessionCookie.split("&").forEach((el) => {
       if (el === "user_id") {
         isNext = true;
       } else if (isNext) {
@@ -37,28 +38,46 @@ export default {
   getProduct(barcode) {
     return axios.get(
       `${OFF_API_URL}/product/${barcode}.json?fields=product_name,brands,ingredients_text,countries_tags,images`
-    )
+    );
   },
 
   getProductUrl(barcode) {
     const lang = getLang();
-    return `https://world${lang === 'en' ? '' : '-' + lang}.openfoodfacts.org/product/${barcode}`
+    return `https://world${
+      lang === "en" ? "" : "-" + lang
+    }.openfoodfacts.org/product/${barcode}`;
   },
 
   getProductEditUrl(barcode) {
     const lang = getLang();
-    return `https://world${lang === 'en' ? '' : '-' + lang}.openfoodfacts.org/cgi/product.pl?type=edit&code=${barcode}`;
+    return `https://world${
+      lang === "en" ? "" : "-" + lang
+    }.openfoodfacts.org/cgi/product.pl?type=edit&code=${barcode}`;
   },
 
   getImageUrl(imagePath) {
-    return combineURLs(OFF_IMAGE_URL, imagePath)
+    return combineURLs(OFF_IMAGE_URL, imagePath);
   },
 
   getTableExtractionAI({ img, x0, y0, x1, y1 }) {
     return `https://off-nutri-test.azurewebsites.net/api/get-nutri-table?name=${img}%7C(${x0},${y0},${x1},${y1})`;
   },
 
-  getNutritionToFillUrl(pageNumber) {
-    return `${OFF_URL}/state/photos-validated/state/nutrition-facts-to-be-completed/${pageNumber}.json?fields=code,lang,image_nutrition_url,product_name,nutriments,images`;
+  // getNutritionToFillUrl(pageNumber, country) {
+  //   return `${OFF_URL}/state/photos-validated/state/nutrition-facts-to-be-completed/${pageNumber}.json?fields=code,lang,image_nutrition_url,product_name,nutriments,images`;
+  // },
+  getNutritionToFillUrl(pageNumber, country, creator) {
+    //
+    return `${OFF_SEARCH}?json=true&fields=code,states,lang,image_nutrition_url,product_name,nutriments,images,creator,countries&action=process&sort_by=last_modified_t&tagtype_0=states&tag_contains_0=contains&tag_0=photos-validated&tagtype_1=states&tag_contains_1=contains&tag_1=nutrition-facts-to-be-completed${
+      country
+        ? `&tagtype_2=countries&tag_contains_2=contains&tag_2=${country}`
+        : ""
+    }${
+      creator
+        ? `&tagtype_${country ? 3 : 2}=creator&tag_contains_${
+            country ? 3 : 2
+          }=contains&tag_${country ? 3 : 2}=${creator}`
+        : ""
+    }`;
   },
 };
--- src/views/NutritionViewComputer.vue
+++ src/views/NutritionViewComputer.vue
@@ -2,6 +2,280 @@
   <div class="ui grid stackable">
     <div class="eight wide column centered">
       <!--four wide column centered-->
+      <div>
+        <select v-model="country">
+          <option value="">country</option>
+          <option>Afghanistan</option>
+          <option>Åland Islands</option>
+          <option>Albania</option>
+          <option>Alderney</option>
+          <option>Algeria</option>
+          <option>American Samoa</option>
+          <option>Andorra</option>
+          <option>Angola</option>
+          <option>Anguilla</option>
+          <option>Antarctic</option>
+          <option>Antarctica</option>
+          <option>Antigua and Barbuda</option>
+          <option>Argentina</option>
+          <option>Armenia</option>
+          <option>Aruba</option>
+          <option>Ascension Island</option>
+          <option>Australia</option>
+          <option>Austria</option>
+          <option>Azerbaijan</option>
+          <option>Bahrain</option>
+          <option>Bangladesh</option>
+          <option>Barbados</option>
+          <option>Belarus</option>
+          <option>Belgium</option>
+          <option>Belize</option>
+          <option>Benin</option>
+          <option>Bermuda</option>
+          <option>Bhutan</option>
+          <option>Bolivia</option>
+          <option>Bonaire</option>
+          <option>Bosnia and Herzegovina</option>
+          <option>Botswana</option>
+          <option>Bouvet Island</option>
+          <option>Brazil</option>
+          <option>British Indian Ocean Territory</option>
+          <option>British Virgin Islands</option>
+          <option>Brunei</option>
+          <option>Bulgaria</option>
+          <option>Burkina Faso</option>
+          <option>Burundi</option>
+          <option>Cambodia</option>
+          <option>Cameroon</option>
+          <option>Canada</option>
+          <option>Cape Verde</option>
+          <option>Caribbean Netherlands</option>
+          <option>Cayman Islands</option>
+          <option>Central African Republic</option>
+          <option>Chad</option>
+          <option>Chile</option>
+          <option>China</option>
+          <option>Christmas Island</option>
+          <option>Cocos (Keeling) Islands</option>
+          <option>Colombia</option>
+          <option>Comoros</option>
+          <option>Cook Islands</option>
+          <option>Costa Rica</option>
+          <option>Côte d'Ivoire</option>
+          <option>Croatia</option>
+          <option>Cuba</option>
+          <option>Curaçao</option>
+          <option>Cyprus</option>
+          <option>Czech Republic</option>
+          <option>Democratic Republic of the Congo</option>
+          <option>Denmark</option>
+          <option>Djibouti</option>
+          <option>Dominica</option>
+          <option>Dominican Republic</option>
+          <option>East Germany</option>
+          <option>Ecuador</option>
+          <option>Egypt</option>
+          <option>El Salvador</option>
+          <option>Equatorial Guinea</option>
+          <option>Eritrea</option>
+          <option>Estonia</option>
+          <option>Ethiopia</option>
+          <option>European Union</option>
+          <option>Falkland Islands</option>
+          <option>Faroe Islands</option>
+          <option>Federated States of Micronesia</option>
+          <option>Fiji</option>
+          <option>Finland</option>
+          <option>France</option>
+          <option>French Guiana</option>
+          <option>French Polynesia</option>
+          <option>French Southern and Antarctic Lands</option>
+          <option>Gabon</option>
+          <option>Galmudug</option>
+          <option>Gambia</option>
+          <option>Georgia</option>
+          <option>Germany</option>
+          <option>Ghana</option>
+          <option>Gibraltar</option>
+          <option>Greece</option>
+          <option>Greenland</option>
+          <option>Grenada</option>
+          <option>Guadeloupe</option>
+          <option>Guam</option>
+          <option>Guatemala</option>
+          <option>Guernsey</option>
+          <option>Guinea</option>
+          <option>Guinea-Bissau</option>
+          <option>Guyana</option>
+          <option>Haiti</option>
+          <option>Heard Island and McDonald Islands</option>
+          <option>Honduras</option>
+          <option>Hong Kong</option>
+          <option>Hungary</option>
+          <option>Iceland</option>
+          <option>India</option>
+          <option>Indonesia</option>
+          <option>Iran</option>
+          <option>Iraq</option>
+          <option>Iraqi Kurdistan</option>
+          <option>Ireland</option>
+          <option>Isle of Man</option>
+          <option>Israel</option>
+          <option>Italy</option>
+          <option>Jamaica</option>
+          <option>Jan Mayen</option>
+          <option>Japan</option>
+          <option>Jersey</option>
+          <option>Jordan</option>
+          <option>Kazakhstan</option>
+          <option>Kenya</option>
+          <option>Kiribati</option>
+          <option>Kosovo</option>
+          <option>Kuwait</option>
+          <option>Kyrgyzstan</option>
+          <option>Laos</option>
+          <option>Latvia</option>
+          <option>Lebanon</option>
+          <option>Lesotho</option>
+          <option>Liberia</option>
+          <option>Libya</option>
+          <option>Liechtenstein</option>
+          <option>Lithuania</option>
+          <option>Luxembourg</option>
+          <option>Macau</option>
+          <option>Madagascar</option>
+          <option>Malawi</option>
+          <option>Malaysia</option>
+          <option>Maldives</option>
+          <option>Mali</option>
+          <option>Malta</option>
+          <option>Marshall Islands</option>
+          <option>Martinique</option>
+          <option>Mauritania</option>
+          <option>Mauritius</option>
+          <option>Mayotte</option>
+          <option>Mexico</option>
+          <option>Moldova</option>
+          <option>Monaco</option>
+          <option>Mongolia</option>
+          <option>Montenegro</option>
+          <option>Montserrat</option>
+          <option>Morocco</option>
+          <option>Mozambique</option>
+          <option>Myanmar</option>
+          <option>Nakhchivan Autonomous Republic</option>
+          <option>Namibia</option>
+          <option>Nauru</option>
+          <option>Nepal</option>
+          <option>Netherlands</option>
+          <option>New Caledonia</option>
+          <option>New Zealand</option>
+          <option>Nicaragua</option>
+          <option>Niger</option>
+          <option>Nigeria</option>
+          <option>Niue</option>
+          <option>Norfolk Island</option>
+          <option>North Korea</option>
+          <option>Northern Mariana Islands</option>
+          <option>Northland State</option>
+          <option>Norway</option>
+          <option>Oman</option>
+          <option>Pakistan</option>
+          <option>Palau</option>
+          <option>Palestinian territories</option>
+          <option>Panama</option>
+          <option>Papua New Guinea</option>
+          <option>Paraguay</option>
+          <option>Peru</option>
+          <option>Philippines</option>
+          <option>Pitcairn</option>
+          <option>Poland</option>
+          <option>Portugal</option>
+          <option>Puerto Rico</option>
+          <option>Qatar</option>
+          <option>Republic of Macedonia</option>
+          <option>Republic of the Congo</option>
+          <option>Republika Srpska</option>
+          <option>Réunion</option>
+          <option>Romania</option>
+          <option>Russia</option>
+          <option>Rwanda</option>
+          <option>Saba</option>
+          <option>Saint Helena</option>
+          <option>Saint Kitts and Nevis</option>
+          <option>Saint Lucia</option>
+          <option>Saint Martin</option>
+          <option>Saint Pierre and Miquelon</option>
+          <option>Saint Vincent and the Grenadines</option>
+          <option>Saint-Barthélemy</option>
+          <option>Samoa</option>
+          <option>San Marino</option>
+          <option>Sao Tomé and Príncipe</option>
+          <option>Saudi Arabia</option>
+          <option>Senegal</option>
+          <option>Serbia</option>
+          <option>Seychelles</option>
+          <option>Sierra Leone</option>
+          <option>Singapore</option>
+          <option>Sint Eustatius</option>
+          <option>Sint Maarten</option>
+          <option>Slovakia</option>
+          <option>Slovenia</option>
+          <option>Solomon Islands</option>
+          <option>Somalia</option>
+          <option>South Africa</option>
+          <option>South Georgia and the South Sandwich Islands</option>
+          <option>South Korea</option>
+          <option>South Sudan</option>
+          <option>Soviet Union</option>
+          <option>Spain</option>
+          <option>Sri Lanka</option>
+          <option>State of Palestine</option>
+          <option>Sudan</option>
+          <option>Suriname</option>
+          <option>Svalbard and Jan Mayen</option>
+          <option>Swaziland</option>
+          <option>Sweden</option>
+          <option>Switzerland</option>
+          <option>Syria</option>
+          <option>Taiwan</option>
+          <option>Tajikistan</option>
+          <option>Tanzania</option>
+          <option>Thailand</option>
+          <option>The Bahamas</option>
+          <option>Timor-Leste</option>
+          <option>Togo</option>
+          <option>Tokelau</option>
+          <option>Tonga</option>
+          <option>Trinidad and Tobago</option>
+          <option>Tunisia</option>
+          <option>Turkey</option>
+          <option>Turkmenistan</option>
+          <option>Turks and Caicos Islands</option>
+          <option>Tuvalu</option>
+          <option>Uganda</option>
+          <option>Ukraine</option>
+          <option>United Arab Emirates</option>
+          <option>United Kingdom</option>
+          <option>United States</option>
+          <option>United States Minor Outlying Islands</option>
+          <option>Uruguay</option>
+          <option>Uzbekistan</option>
+          <option>Vanuatu</option>
+          <option>Vatican City</option>
+          <option>Venezuela</option>
+          <option>Vietnam</option>
+          <option>Virgin Islands of the United States</option>
+          <option>Wallis and Futuna</option>
+          <option>Western Sahara</option>
+          <option>World</option>
+          <option>Yemen</option>
+          <option>Yugoslavia</option>
+          <option>Zambia</option>
+          <option>Zimbabwe</option>
+        </select>
+        <input v-model="contributor" placeholder="contributor" />
+      </div>
       <div v-if="bufferIsEmpty">
         <p v-if="loading">Loading ...</p>
         <p v-else>No remaining images</p>
@@ -151,11 +425,13 @@ import "vue-advanced-cropper/dist/style.css";
 import InteractiveImage from "../components/InteractiveImage";
 import NutritionTable from "../components/NutritionTable";
 
-const getProducts = async (nbOfPages) => {
+const getProducts = async (nbOfPages, country = "", contributor = "") => {
   const randomPage = 1 + Math.floor(Math.random() * nbOfPages);
   const {
     data: { products },
-  } = await axios(offService.getNutritionToFillUrl(randomPage));
+  } = await axios(
+    offService.getNutritionToFillUrl(randomPage, country, contributor)
+  );
   return products;
 };
 
@@ -181,6 +457,8 @@ export default {
       step: 0,
       nutritionPredictionIndex: 0,
       nutrimentToFillIndex: 0,
+      country: "",
+      contributor: "",
     };
   },
   computed: {
@@ -535,10 +813,18 @@ export default {
         maxY: coordinates.top + coordinates.height,
       };
     },
-    addProducts: async function() {
+    addProducts: async function(country, contributor) {
       this.loading = true;
-      const newProducts = await getProducts(100);
+      const newProducts = await getProducts(100, country, contributor);
+
       this.productBuffer = this.productBuffer.concat(newProducts);
+      this.loading = false;
+    },
+    replaceProducts: async function(country, contributor) {
+      this.loading = true;
+      const newProducts = await getProducts(1, country, contributor);
+
+      this.productBuffer = [...newProducts];
       this.loading = false;
     },
     skipProduct() {
@@ -623,7 +909,7 @@ export default {
       // watch when a new question is asked
       this.resetProductData();
       if (this.productBuffer.length <= 5 && !this.loading) {
-        this.addProducts();
+        this.addProducts(this.country, this.contributor);
       }
       if (
         this.productBuffer[0] &&
@@ -673,6 +959,40 @@ export default {
         this.nutrimentToFillIndex =
           this.nutrimentToFillIndex % this.nutriKeys.length || 0;
       }
+    },
+    country: function() {
+      const vm = this;
+
+      const currentCountry = this.country;
+      const currentContributor = this.contributor;
+
+      setTimeout(() => {
+        if (
+          currentCountry === vm.country &&
+          currentContributor === vm.contributor
+        ) {
+          this.replaceProducts(vm.country, vm.contributor);
+        } else {
+          console.log("change");
+        }
+      }, 500);
+    },
+    contributor: function() {
+      const vm = this;
+
+      const currentCountry = this.country;
+      const currentContributor = this.contributor;
+
+      setTimeout(() => {
+        if (
+          currentCountry === vm.country &&
+          currentContributor === vm.contributor
+        ) {
+          this.replaceProducts(vm.country, vm.contributor);
+        } else {
+          console.log("change");
+        }
+      }, 500);
     },
   },
   mounted: function() {
