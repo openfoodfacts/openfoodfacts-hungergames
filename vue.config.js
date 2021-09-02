1947bed758ada85812d6d523510d5324df83d73c
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2021-09-02 17:16:42 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,4 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/" + process.env.CI_PROJECT_NAME + "/"
-      : "/",
 };
