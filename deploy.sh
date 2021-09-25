077080e996824a9ab436fed776e23cf519ae6f66
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-09-25 23:54:19 +0200

    replicate index.html to avoid 404 error t first loading

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd dist;
+cp index.html 404.html;
+cp index.html insights.html;
+cp index.html questions.html;
+cp index.html eco-score.html;
+cp index.html secretTools.html;
+cp index.html nutritions.html;
+cp index.html nutritionInteractive.html;
+cp index.html table.html;
+cp index.html settings.html;
+cp index.html logos.html;
+cd ..;
