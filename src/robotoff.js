2170d5773f2b47f8b69466f7e1cd0d4eeac955b8
Jagruti Tiwari <tiwarijagruti1@gmail.com>
2022-03-20 19:32:16 +0530

    Update src/robotoff.js
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- src/robotoff.js
+++ src/robotoff.js
@@ -129,7 +129,7 @@ export default {
   },
 
   getNutritionValueFromImage(imageUrl) {
-    alert(' in robotoff.getNutritionValueFromImage')
+    console.log(' in robotoff.getNutritionValueFromImage')
     var subStringUrl = imageUrl.substr(0, imageUrl.lastIndexOf("/"));
     var url = `${ROBOTOFF_API_URL}/predict/nutrient?ocr_url=${subStringUrl}/10.json`
 
